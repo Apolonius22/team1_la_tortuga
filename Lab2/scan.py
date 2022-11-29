7cca48cbe12ac2cdaa787a9c094536f99e60bf40
HAMZAH ALHAMIDI <46681005+Hamzah0o@users.noreply.github.com>
2022-11-29 14:51:44 +0100

    Update scan.py

--- Lab2/scan.py
+++ Lab2/scan.py
@@ -22,56 +22,17 @@ def scan_callback(msg):
     y_l = []
 
     for value in ranges:
-        angle_counter+= angle_increment
-
-     
+        angle_counter+= angle_increment  
         if type(value) == float:
 
             x_l.append(-1 * (value * sin(angle_counter)))
             y_l.append(value * cos(angle_counter))
-
-
-
-
-            # if angle_counter <= 90:
-            #     print(cur_rot_z)
-            #     # print(f"value ={value}")
-            #     # print(f"angle_counter ={angle_counter}")
-            #     x_l.append(-1 * (value * sin(angle_counter*pi/180)))
-            #     y_l.append(value * cos(angle_counter*pi/180))
-            
-            # elif angle_counter <= 180:
-            #     x_l.append(-1 * (value * cos((angle_counter -90)*(pi/180))))
-            #     y_l.append(-1*(value * sin((angle_counter- 90)*(pi/180))))
-            
-            # elif angle_counter <= 270:
-            #     x_l.append( (value * sin((angle_counter -180) *pi/180)))
-            #     y_l.append(-1*(value * cos((angle_counter-180)*pi/180 )))
-            
-            # elif angle_counter <= 360:
-            #     x_l.append(value * cos((angle_counter-270) *pi/180))
-            #     y_l.append(value * sin((angle_counter-270)*pi/180))
     if not shown:
-        print(cur_rot_z)
-        
+        print(cur_rot_z)        
         y_pos = 0
         x_pos = 0
         x_direct = 0
         y_direct = 1
-        # if cur_rot_z > 0:
-        #     x_direct = - sin(cur_rot_z)
-        #     y_direct = cos(cur_rot_z)
- 
-        # elif cur_rot_z < 0:
-        #     print(cur_rot_z)
-        #     print(2 * pi + cur_rot_z)
-        #     print(cos(2 * pi + cur_rot_z))
-        #     print(- sin(2 * pi + cur_rot_z))
-        #     x_direct = - sin(2 * pi + cur_rot_z)
-        #     y_direct = - cos(2 * pi + cur_rot_z)
-
-
-
         # Creating plot
         fig, ax = plt.subplots(figsize = (12, 12))
         ax.quiver(x_pos, y_pos, x_direct, y_direct)
@@ -80,7 +41,6 @@ def scan_callback(msg):
         if not shown:
             plt.show()
             shown = True
-
 def odometry_callback(msg):
     global cur_rot_z
     cur_position = msg.pose.pose.position
@@ -88,11 +48,6 @@ def odometry_callback(msg):
     cur_rpy = tftr.euler_from_quaternion((cur_q.x, cur_q.y, cur_q.z, cur_q.w))  # roll pitch yaw
     cur_rot_z = cur_rpy[2]
     
-
-            
-        
-        
-        # print(value_x)
 ranges = []
 dis_foward = 0.0
 #set sub,pub
@@ -100,6 +55,5 @@ rospy.init_node('object_detection')
 
 scan_sub = rospy.Subscriber('/scan',LaserScan,scan_callback)
 sub_odom = rospy.Subscriber("/odom", Odometry,odometry_callback)
-#RATE = rospy.get_param('/rate', 50)
 while not rospy.is_shutdown():
     pass
